4e777f6b268b5bd2eb8ba28e360d072a94635d81
向家红 <541353071@qq.com>
2016-06-15 20:05:34 +0800

    *

--- js/jquery.plugins.js
+++ js/jquery.plugins.js
@@ -1,9 +1,7 @@
 //common plugins
-
 (function($){
 	$().ready(function(){
-
-		//designer dynamic设计师动态
+		//designer dynamic
 		$.fn.extend({
 		    designDynamic:function(time,options){
 
@@ -33,12 +31,83 @@
 						'top':'-='+options.dynamicListH
 					})
 				}
-
 			},time);
 
             return this;
-		    }
-		});
+		    },
 
+		    //Seamless scrolling
+		    seamlessRoll:function(options){
+
+				$('ul',this).append($('ul li',this).clone());
+	            var scrollWidth=this.width();  //总共包裹的slide宽度
+	            var scrollLiWidth=$('ul li',this).outerWidth(true);  
+	            var scrollLiLength=$('ul li',this).length;  
+	            var scrollObj=$('ul',this);
+	            var scrollTotleWidth=scrollLiLength*scrollLiWidth;
+	            $('ul',this).css('width',scrollTotleWidth);
+
+	            var defaults = {
+	                direction: 'left',
+	                type:'auto',
+	                scrollAmount:'1',
+	                scrollDelay:'2500',
+	                scrollDistance:scrollLiWidth,   //步长
+	                minLength:3,  //最小个数
+	                animateTime:''
+	            };
+
+	            var options = $.extend(defaults, options);
+
+	            function slideAuto(){
+	        		if(scrollObj.css('left').replace('px','')<=-(scrollTotleWidth/2)){
+	        			console.log('a');
+	            		scrollObj.css({
+		            		'left':'0'
+		            	})
+	            	}else{
+	            		scrollObj.animate({
+		            		'left':'-='+options.scrollDistance+'px'
+		            	},options.animateTime)
+	            	}
+	        		
+	            }
+
+	            slideAuto();
+	           
+	            if(scrollLiLength>options.minLength){
+	            	var slideScroll=setInterval(slideAuto,options.scrollDelay);
+	            	scrollObj.hover(function(){
+		            	clearInterval(slideScroll);
+		            },function(){
+		            	clearInterval(slideScroll);
+		            	slideScroll=setInterval(slideAuto,options.scrollDelay);
+		            })
+				}
+
+	            return this;
+			},
+
+			//toggle hide
+			//## .toggleCon ##
+			//## .toggle - .hide ##
+
+			//##<div class="designCenter toggleCon">
+			//##	<div class="toggle"></div>
+			//##	<div class="toggle hide"></div>
+			//##	<div class="toggle hide"></div>
+			//##</div>
+			toggleHide:function(){
+				var toggleWrap=$('.toggleCon');
+				$('a',this).on('click',function(){
+					var $index=$(this).index();
+					$(this).addClass('active').siblings().removeClass('active');
+
+					$('.toggle',toggleWrap).addClass('hide');
+					$('.toggle',toggleWrap).eq($index).removeClass('hide');
+				})
+			}
+
+		});
 	})
 })(jQuery);
