55abb9e25e2814790aeccfa4d6e70d2ebe6354ac
向家红 <541353071@qq.com>
2016-06-20 14:13:55 +0800

    *

--- js/jquery.plugins.js
+++ js/jquery.plugins.js
@@ -149,14 +149,28 @@
 			//##	<div class="toggle hide"></div>
 			//##	<div class="toggle hide"></div>
 			//##</div>
-			toggleHide:function(){
+			toggleHide:function(options){
+				var defaults = {
+	                hash: false
+	            };
+	            var options = $.extend(defaults, options);
+
 				var toggleWrap=$('.toggleCon');
 				$('a',this).on('click',function(){
+
+
 					var $index=$(this).index();
 					$(this).addClass('active').siblings().removeClass('active');
 
 					$('.toggle',toggleWrap).addClass('hide');
 					$('.toggle',toggleWrap).eq($index).removeClass('hide');
+
+					if(options.hash){
+						window.location.hash='#'+($index+1);
+					}
+					
+
+
 				})
 			}
 
